7f2d706d2879bee47e7b17c4aa4ccfc9c13134f3
Vidisha Srivastav <vidisha.srivastav16@gmail.com>
2018-06-23 09:46:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+ 
+  //create template function
+  var templateFn = _.template(commentTemplate);
+ 
+  var commentsDiv = document.getElementById("comments");
+ 
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+ 
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+}
